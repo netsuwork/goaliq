b6b81f20e91d6fe965388188f00d47ca867afc51
netsuwork <81774632+netsuwork@users.noreply.github.com>
2026-05-16 22:38:12 +0300

    Update api.js

--- frontend/public/js/api.js
+++ frontend/public/js/api.js
@@ -1,6 +1,5 @@
 // GoalIQ API Client
-const API_BASE = 'https://goaliq-api.onrender.com/api';
-
+const API_BASE = 'https://goaliq.onrender.com/api';
 const api = {
   _token: localStorage.getItem('goaliq_token'),
 
